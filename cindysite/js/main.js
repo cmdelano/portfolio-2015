651c9afeeac1eea71e57329150fd6e7ecfa32a52
cmdelano <cmdelano@gmail.com>
2015-07-21 09:40:37 -0400

    initial commit for cindydelano.com

---
+++ cindysite/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+$("img.rollover").hover( 
+function() { this.src = this.src.replace("_off", "_on"); 
+}, 
+function() { this.src = this.src.replace("_on", "_off"); 
+});
+}); 
+
